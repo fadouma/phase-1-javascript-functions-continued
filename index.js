4c8deb254465a073417af2c29e70566f12304464
fadouma ahmed <fannyahmed85@gmail.com>
2024-09-25 21:22:55 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activities = "roller-skate") {
+    console.log('This Saturday, I want to roller-skate!');
+return `This Saturday, I want to ${activities}!`
+}
+function mondayWork(activities = "go to the office"){
+console.log(`This Monday, I will ${activities}.`);
+return `This Monday, I will ${activities}.`
+}
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+return `You are ${flair}${adjective}${flair}!`
+    }
+}
